b7546f039dafbfe7cfbba58a07754fef58de58f7
Mateus de Souza Petris <226363@seadh.es.gov.br.local>
2026-03-04 17:18:23 -0300

    modelagem do banco de dados,NinjaModel e MissoesModel Concluidos, anotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,57 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    // @OneToMany uma missão pode ter varios ninjas
+    @OneToMany
+    private List<NinjaModel> ninjas;
+
+    public MissoesModel(){
+
+    }
+
+    public MissoesModel(String nome, String dificuldade){
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // Entity ele transforma uma classe em uma entidade do DB
 // JPA = Java Persistence API
@@ -11,9 +14,16 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    // @ManyToOne um ninja tem uma unica missão
+    @ManyToOne
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
