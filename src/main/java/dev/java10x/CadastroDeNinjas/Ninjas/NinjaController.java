6579fb2a5e4ec110794ced4158dc896baa027495
Mateusssi <mateuspetris@gmail.com>
2026-08-03 21:14:15 -0300

    feat: Inclui a rota de cadastro de ninjas usando POST method, tambem inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -23,8 +23,8 @@ public class NinjaController {
 
     // Adicionar um Ninja
     @PostMapping("/criar")
-    public String criarNinja(){
-        return "Ninja Criado";
+    public NinjaModel criarNinja(@RequestBody NinjaModel ninja){
+        return ninjaService.criarNinja(ninja);
     }
 
     // Mostrar todos os ninjas (READ)
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -25,4 +25,8 @@ public class NinjaService {
         return ninjaPorId.orElse(null);
     }
 
+    public NinjaModel criarNinja(NinjaModel ninja){
+        return ninjaRepository.save(ninja);
+    }
+
 }
