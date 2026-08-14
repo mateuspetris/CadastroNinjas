05863fe1f26eaff1ff2f544184c86e67fe0495cc
Mateusssi <mateuspetris@gmail.com>
2026-08-14 12:43:45 -0300

    feat: Adiciona a rota de deleção do ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -46,8 +46,8 @@ public class NinjaController {
     }
 
     // Deletar Ninjas (DELETE)
-    @DeleteMapping("/deletarID")
-    public String deletarNinjaPorID(){
-        return "Ninja deletado por ID";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarNinjaPorID(@PathVariable Long id){
+        ninjaService.deletarNinjaPorId(id);
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Service
 public class NinjaService {
 
-    private NinjaRepository ninjaRepository;
+    final private NinjaRepository ninjaRepository;
 
     public NinjaService(NinjaRepository ninjaRepository){
         this.ninjaRepository = ninjaRepository;
@@ -20,13 +20,17 @@ public class NinjaService {
     }
 
     // Listar por id
-    public NinjaModel listarPorId(long id){
+    public NinjaModel listarPorId(Long id){
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
     }
 
     public NinjaModel criarNinja(NinjaModel ninja){
         return ninjaRepository.save(ninja);
+    }
+
+    public void deletarNinjaPorId(Long id){
+        ninjaRepository.deleteById(id);
     }
 
 }
