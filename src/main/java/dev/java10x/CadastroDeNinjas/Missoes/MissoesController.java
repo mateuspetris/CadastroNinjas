d43047fdabac3ded06f1017585b78b8e9174d0c8
Mateusssi <mateuspetris@gmail.com>
2026-09-01 19:33:15 -0300

    refactor: Adiciona aos controllers status http e body utilizando o tipo ResponseEntity

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,6 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaDTO;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,25 +20,30 @@ public class MissoesController {
     }
 
     @GetMapping("/listar")
-    public List<MissoesModel> listarMissao(){
-        return missoesService.listarMissoes();
+    public ResponseEntity<List<MissoesDTO>> listarMissao(){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        return ResponseEntity.ok(missoes);
     }
 
     // Post -- Mandar uma requisao para criar as missoes
     @PostMapping("/criar")
-    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
-        return missoesService.criarMissao(missao);
+    public ResponseEntity<String> criarMissao(@RequestBody MissoesDTO missao){
+        MissoesDTO missoes = missoesService.criarMissao(missao);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Missão criada com sucesso: " + "{ID} - " + missoes.getId() + "\n" +"Nome: " + missoes.getNome());
     }
 
     // Put -- Mandar uma requisao para alterar as missoes
     @PutMapping("/alterar/{id}")
-    public MissoesModel alterarMissao(@PathVariable Long id, @RequestBody MissoesModel missaoAtualizada){
-        return missoesService.alterarMissao(id, missaoAtualizada);
+    public ResponseEntity<MissoesDTO> alterarMissao(@PathVariable Long id, @RequestBody MissoesDTO missaoAtualizada){
+        MissoesDTO missoes = missoesService.alterarMissao(id, missaoAtualizada);
+        return ResponseEntity.ok(missoes);
     }
 
     // Delete -- Mandar uma requisao para deletar as missoes
-    @DeleteMapping("/deletar")
-    public void deletarMissao(@PathVariable long id){
-        missoesService.deleterMissaoId(id);
+    @DeleteMapping("/deletar/{ID}")
+    public ResponseEntity<String> deletarMissao(@PathVariable long ID){
+        missoesService.deleterMissaoId(ID);
+        return ResponseEntity.ok("Missão deletada com sucesso");
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,35 +1,58 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.exceptions.IdNotFoundException;
+import dev.java10x.CadastroDeNinjas.exceptions.MissoesNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
 
     private final MissoesRepository missoesRepository;
+    private final MissoesMapper missoesMapper;
 
-    public MissoesService(MissoesRepository missoesRepository){
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
-    public List<MissoesModel> listarMissoes(){
-        return missoesRepository.findAll();
-    }
-
-    public MissoesModel criarMissao(MissoesModel missoes){
-        return missoesRepository.save(missoes);
-    }
-
-    public MissoesModel alterarMissao(Long id, MissoesModel missaoAtualizada){
-        if(missoesRepository.existsById(id)) {
-            missaoAtualizada.setId(id);
-            return missoesRepository.save(missaoAtualizada);
+    public List<MissoesDTO> listarMissoes(){
+        List<MissoesModel> missoes = missoesRepository.findAll();
+        if(missoes.isEmpty()){
+            throw new IdNotFoundException();
         }
-        return null;
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public MissoesDTO criarMissao(MissoesDTO missoesDTO){
+        MissoesModel missao = missoesMapper.map(missoesDTO);
+        if(missao == null){
+            throw new MissoesNotFoundException();
+        }
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao);
+    }
+
+    public MissoesDTO alterarMissao(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missoes = missoesRepository.findById(id);
+        if(missoes.isEmpty()){
+            throw new IdNotFoundException();
+        }
+        MissoesModel missaoAtualizado = missoesMapper.map(missoesDTO);
+        missaoAtualizado.setId(id);
+        MissoesModel missaoSalva = missoesRepository.save(missaoAtualizado);
+        return missoesMapper.map(missaoSalva);
     }
 
     public void deleterMissaoId(Long id){
+        if(missoesRepository.findById(id).isEmpty()){
+            throw new IdNotFoundException();
+        }
         missoesRepository.deleteById(id);
     }
 
